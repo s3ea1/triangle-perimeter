078c15782a31398aa44183afc92b1d02dfde361e
s3ea1 <130776389+s3ea1@users.noreply.github.com>
2024-02-07 11:29:01 -0700

    Add files via upload

---
+++ triangle-perimeter-start-main/main.js
@@ -0,0 +1,20 @@
+// Triangle Perimeter Assignment Start Code
+
+// HTML Element Variable
+let outputEL = document.getElementById("output");
+
+// Button Click Listener
+document.getElementById("btn").addEventListener("click", btnclicked);
+
+function btnclicked() {
+  // Input
+  let output1 = +document.getElementById("output1").value;
+  let output2 = +document.getElementById("output2").value;
+  let output3 = +document.getElementById("output3").value;
+  let calculate = document.getElementById("calculate").value;
+
+  // Process
+  let Calculate = output1 + output2 + output3;
+
+  // Output
+}
